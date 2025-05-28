eb6a370f42f99b910f753d1fee5845461f93df28
Pau Morillas <morillashuertapau@gmail.com>
2025-05-28 18:47:30 +0200

    Commit inicial - Base del proyecto funcional con index

---
+++ src/main/java/com/example/demo/repository/entity/Categoria.java
@@ -0,0 +1,43 @@
+package com.example.demo.repository.entity;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "cat_name")
+	private String name;
+	@Column(name = "cat_description")
+	private String description;
+
+	// Representa la relacion
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+	private ArrayList<Post> postsList;
+
+	public Categoria() {
+		this.postsList = new ArrayList<>();
+	}
+
+	public Categoria(Long id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.name = nombre;
+		this.description = descripcion;
+		this.postsList = new ArrayList<>();
+	}
+
+}
---
+++ src/main/java/com/example/demo/security/UserDetailsServiceConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserDetailsServiceConfig {
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        var user = User.withUsername("pawete")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
